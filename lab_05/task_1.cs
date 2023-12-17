07f530a6a4c43fd66069b0363337357fe15238c4
unknown <fedorinter2004@gmail.com>
2023-12-17 22:14:10 +0300

    v0.0.2

--- lab_05/task_1.cs
+++ lab_05/task_1.cs
@@ -1,2 +1,155 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MyMatrix
+{
+    private int[,] matrix;
+    private int rows;
+    private int columns;
+    private Random random = new Random();
+
+    public MyMatrix(int rows, int columns, int minValue, int maxValue)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        matrix = new int[rows, columns];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix[i, j] = random.Next(minValue, maxValue + 1);
+            }
+        }
+    }
+
+    public void ChangeSize(int newRows, int newColumns, int minValue, int maxValue)
+    {
+        if (newRows == rows && newColumns == columns)
+            return;
+
+        int[,] newMatrix = new int[newRows, newColumns];
+
+        for (int i = 0; i < Math.Min(rows, newRows); i++)
+        {
+            for (int j = 0; j < Math.Min(columns, newColumns); j++)
+            {
+                newMatrix[i, j] = matrix[i, j];
+            }
+        }
+
+        if (newRows > rows || newColumns > columns)
+        {
+            for (int i = 0; i < newRows; i++)
+            {
+                for (int j = 0; j < newColumns; j++)
+                {
+                    if (i >= rows || j >= columns)
+                    {
+                        newMatrix[i, j] = random.Next(minValue, maxValue);
+                    }
+                }
+            }
+        }
+
+        matrix = newMatrix;
+        rows = newRows;
+        columns = newColumns;
+    }
+
+    public void PrintPartialy(int startRow, int endRow, int startColumn, int endColumn)
+    {
+        for (int i = startRow; i <= endRow && i < rows; i++)
+        {
+            for (int j = startColumn; j <= endColumn && j < columns; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void Print()
+    {
+        PrintPartialy(0, rows - 1, 0, columns - 1);
+    }
+
+    public int this[int index1, int index2]
+    {
+        get
+        {
+            if (index1 >= 0 && index1 - 1 < rows && index2 >= 0 && index2 - 1 < columns)
+            {
+                return matrix[index1 - 1, index2 - 1];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Индекс находится вне диапазона");
+            }
+        }
+        set
+        {
+            if (index1 >= 0 && index1 - 1 < rows && index2 >= 0 && index2 - 1 < columns)
+            {
+                matrix[index1 - 1, index2 - 1] = value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Индекс находится вне диапазона");
+            }
+        }
+    }
+}
+
+class lab_05_1
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите минимальное значение: ");
+        int minValue = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите максимальное значение: ");
+        int maxValue = int.Parse(Console.ReadLine());
+
+        MyMatrix matrix = new MyMatrix(rows, columns, minValue, maxValue);
+
+        Console.WriteLine("Матрица:");
+        matrix.Print();
+
+        Console.WriteLine("\nИзменение размера матрицы:");
+        Console.Write("Введите новое число строк: ");
+        int newRows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите новое число столбцов: ");
+        int newColumns = int.Parse(Console.ReadLine());
+
+        matrix.ChangeSize(newRows, newColumns, minValue, maxValue);
+
+        Console.WriteLine("Новая матрица:");
+        matrix.Print();
+
+        Console.WriteLine("\nОтоброжение части матрицы 3x3:");
+        matrix.PrintPartialy(0, 2, 0, 2);
+
+        Console.WriteLine("\nИзменение элемента матрицы:");
+        Console.Write("Введите индекс строки: ");
+        int rowIndex = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите индекс столбца: ");
+        int columnIndex = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите новое значение: ");
+        int newValue = int.Parse(Console.ReadLine());
+
+        matrix[rowIndex, columnIndex] = newValue;
+
+        Console.WriteLine("Измененая матрица:");
+        matrix.Print();
+    }
+
+}
--- lab_05_2/task_2.cs
+++ lab_05_2/task_2.cs
@@ -1,2 +1,122 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MyList<T> : IEnumerable<T>
+{
+    private T[] items;
+    private int count;
+
+
+
+    public MyList()
+    {
+        items = new T[0];
+        count = 0;
+    }
+
+    public MyList(params T[] initialValues)
+    {
+        items = new T[initialValues.Length];
+        Array.Copy(initialValues, items, initialValues.Length);
+        count = initialValues.Length;
+    }
+
+    public MyList(IEnumerable<T> collection)
+    {
+        items = new T[0];
+        foreach (var item in collection)
+        {
+            Add(item);
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (count == items.Length)
+        {
+            int newCapacity = count == 0 ? 4 : count * 2;
+            T[] newItems = new T[newCapacity];
+            Array.Copy(items, newItems, count);
+            items = newItems;
+        }
+
+        items[count] = item;
+        count++;
+    }
+
+    public T this[int index]
+    {
+        get
+        {
+            if (index < 0 || index >= count)
+                throw new IndexOutOfRangeException();
+
+            return items[index];
+        }
+    }
+
+    public int Count
+    {
+        get { return items.Length; }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return items[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class lab_05_2
+{
+    static void Main()
+    {
+        MyList<int> myList = new MyList<int> { 1, 2, 3, 4, 5 };
+
+
+        Console.WriteLine($"\nОбщее количество элементов: {myList.Count}");
+
+        int Capasity = 0;
+
+
+        for (int i = 0; i < 100; i++)
+        {
+            myList.Add(i);
+            if (myList.Count != Capasity)
+            {
+                Console.WriteLine($"\nОбщее количество элементов: {myList.Count}");
+                Capasity = myList.Count;
+            }
+
+
+        }
+
+        Console.WriteLine($"Ваш элемент i - 1:\n");
+        foreach (var item in myList)
+        {
+            Console.WriteLine($"Элемент: {item}");
+        }
+
+        //foreach (var item in myList)
+        //{
+        //    for (int i = 0; i < 1000000; ++i)
+        //    {
+        //        myList.Add(i);
+        //    }
+        //    if (myList.Count == 5)
+        //    {
+        //        Console.WriteLine("Количетсво элементов не изменилось");
+        //    }
+        //    else Console.WriteLine("Количетсво элементов  изменилось");
+
+        Console.WriteLine($"\nОбщее количество элементов: {myList.Count}");
+    }
+}
